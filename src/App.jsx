4f647ffde30a15532fcd40837dcecf7dda566473
Jesse Page <jesse.page@corebts.com>
2023-02-10 16:46:13 -0600

    dice numbers randomized and styled

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,29 @@ import React from "react"
 import Die from "./components/Die"
 
 export default function App() {
+
+    const [dice, setDice] = React.useState(allNewDice())
+
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
+    }
+
+    function handleClick(){
+        setDice(allNewDice())
+    }
+     
+    const diceElements = dice.map(die => <Die value={die} />)
+
     return (
         <main>
             <div className="dice-container">
-                <Die value='1' />
-                <Die value='2' />
-                <Die value='3' />
-                <Die value='4' />
-                <Die value='5' />
-                <Die value='6' />
-                <Die value='1' />
-                <Die value='2' />
-                <Die value='3' />
-                <Die value='4' />
+                {diceElements}
             </div>
+            <button className="roll-dice" onClick={handleClick}>Roll!</button>
         </main>
     )
 }
